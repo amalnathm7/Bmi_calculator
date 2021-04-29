b0e2f03d06ecc1b44a9c41c38645272fa35950bc
amalnathm7 <amalnathm7@gmail.com>
2021-04-29 23:23:56 +0530

    Release

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import "package:flutter/material.dart";
+import 'package:lottie/lottie.dart';
 
 void main() {
   runApp(MyApp());
@@ -41,42 +42,48 @@ class BMIState extends State<BMI> {
 
       bmi = (weight / height / height) * 10000;
 
-      if(_gender == Gender.Male){
-        if(bmi < 18.5)
+      if (_gender == Gender.Male) {
+        if (bmi < 18.5)
           str = "assets/men_under.jpg";
-        else if(bmi <= 24.9)
+        else if (bmi <= 24.9)
           str = "assets/men_normal.jpg";
-        else if(bmi <= 30)
+        else if (bmi <= 30)
           str = "assets/men_over.jpg";
-        else if(bmi <= 34.9)
+        else if (bmi <= 34.9)
           str = "assets/men_obese.jpg";
         else
           str = "assets/men_extreme.jpg";
       } else {
-        if(bmi < 18.5)
+        if (bmi < 18.5)
           str = "assets/women_under.jpg";
-        else if(bmi <= 24.9)
+        else if (bmi <= 24.9)
           str = "assets/women_normal.jpg";
-        else if(bmi <= 30)
+        else if (bmi <= 30)
           str = "assets/women_over.jpg";
-        else if(bmi <= 34.9)
+        else if (bmi <= 34.9)
           str = "assets/women_obese.jpg";
         else
           str = "assets/women_extreme.jpg";
       }
-      
+
       showDialog(
           context: context,
-          builder: (BuildContext context){
+          builder: (BuildContext context) {
             return AlertDialog(
-              title: Text("Your BMI = "+bmi.toStringAsPrecision(5), textAlign: TextAlign.center,),
+              title: Text(
+                "Your BMI = " + bmi.toStringAsPrecision(5),
+                textAlign: TextAlign.center,
+                style:
+                    TextStyle(fontWeight: FontWeight.bold, color: Colors.grey),
+              ),
               content: Container(
                 height: 200,
                 child: Image.asset(str),
-              )
+              ),
             );
-          }
-      );
+          });
+      txt1.clear();
+      txt2.clear();
     } catch (e) {}
   }
 
@@ -86,42 +93,44 @@ class BMIState extends State<BMI> {
         backgroundColor: Colors.orange[100],
         appBar: AppBar(
           backgroundColor: Colors.orange[700],
-          title: Text("BMI Calculator",
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 20,
-                fontStyle: FontStyle.italic
-              ),
-            ),
+          title: Text(
+            "BMI Calculator",
+            style: TextStyle(
+                color: Colors.white, fontSize: 20, fontStyle: FontStyle.italic),
           ),
+        ),
         body: SingleChildScrollView(
           child: Padding(
             padding: EdgeInsets.all(20),
             child: Column(
               children: [
                 SizedBox(
-                  height: 20,
+                  height: 10,
                 ),
-                Text("Let's see if you're ",
+                Text(
+                  "Let's see if you're ",
                   textAlign: TextAlign.center,
                   style: TextStyle(
                       fontStyle: FontStyle.italic,
                       fontWeight: FontWeight.bold,
                       fontSize: 28,
-                      color: Colors.deepOrangeAccent
-                  ),
+                      color: Colors.deepOrangeAccent),
                 ),
-                Text("healthy!",
+                Text(
+                  "healthy!",
                   textAlign: TextAlign.center,
                   style: TextStyle(
                       fontStyle: FontStyle.italic,
                       fontWeight: FontWeight.bold,
                       fontSize: 28,
-                      color: Colors.lightGreen
-                  ),
+                      color: Colors.lightGreen),
+                ),
+                Container(
+                  height: 100,
+                  child: Lottie.asset("assets/meter.json"),
                 ),
                 SizedBox(
-                  height: 70,
+                  height: 40,
                 ),
                 TextField(
                   controller: txt1,
@@ -133,7 +142,7 @@ class BMIState extends State<BMI> {
                           borderSide: BorderSide(color: Colors.orange))),
                 ),
                 SizedBox(
-                  height: 30,
+                  height: 20,
                 ),
                 TextField(
                   controller: txt2,
@@ -145,20 +154,19 @@ class BMIState extends State<BMI> {
                           borderSide: BorderSide(color: Colors.orange))),
                 ),
                 SizedBox(
-                  height: 30,
+                  height: 20,
                 ),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Container(
-                      width: 150,
+                    Expanded(
                       child: ListTile(
                         title: Text("Male"),
                         leading: Radio<Gender>(
                           activeColor: Colors.orange,
                           value: Gender.Male,
                           groupValue: _gender,
-                          onChanged: (Gender value){
+                          onChanged: (Gender value) {
                             setState(() {
                               _gender = value;
                             });
@@ -166,15 +174,14 @@ class BMIState extends State<BMI> {
                         ),
                       ),
                     ),
-                    Container(
-                      width: 150,
+                    Expanded(
                       child: ListTile(
                         title: Text("Female"),
                         leading: Radio<Gender>(
                           activeColor: Colors.orange,
                           value: Gender.Female,
                           groupValue: _gender,
-                          onChanged: (Gender value){
+                          onChanged: (Gender value) {
                             setState(() {
                               _gender = value;
                             });
@@ -185,14 +192,17 @@ class BMIState extends State<BMI> {
                   ],
                 ),
                 SizedBox(
-                  height: 50,
+                  height: 30,
                 ),
                 ElevatedButton(
                     onPressed: calc,
                     style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all(Colors.deepOrange),
-                      padding: MaterialStateProperty.all(EdgeInsets.only(left: 3, right: 3,))
-                    ),
+                        backgroundColor:
+                            MaterialStateProperty.all(Colors.deepOrange),
+                        padding: MaterialStateProperty.all(EdgeInsets.only(
+                          left: 3,
+                          right: 3,
+                        ))),
                     child: TextButton.icon(
                       label: Text(
                         "Calculate BMI",
@@ -201,18 +211,27 @@ class BMIState extends State<BMI> {
                           fontSize: 20,
                         ),
                       ),
-                      icon: Icon(Icons.speed, color: Colors.black45,),
+                      icon: Icon(
+                        Icons.speed,
+                        color: Colors.black45,
+                      ),
                     )),
-                SizedBox(
-                  height: 70,
-                ),
-                Text("Copyright @ Amal Nath. All Rights Reserved.",
-                  style: TextStyle(
-                    fontSize: 10
-                  ),)
               ],
             ),
           ),
-        ));
+        ),
+      bottomNavigationBar: BottomAppBar(
+        elevation: 0,
+        color: Colors.transparent,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Text(
+            "Copyright @ Amal Nath. All Rights Reserved.",
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 10),
+          ),
+        ),
+      ),
+    );
   }
 }
